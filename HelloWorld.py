8b89f4945bc71213df2de40e3e9336027d60cf62
rishi2o2o <rishi.rvv@gmail.com>
2023-10-27 17:41:30 +0530

    commit1

---
+++ HelloWorld.py
@@ -0,0 +1,3 @@
+# This Python program will print Hello World...
+
+print("Hello World...\n")
---
+++ Prog1.py
@@ -0,0 +1,4 @@
+# Source code for summation for two numbers
+
+def summation(data):
+    return sum(data)
